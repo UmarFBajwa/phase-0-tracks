0a308d6030b43da9ce67fbb2bf0b0e6a9679c338
UmarFBajwa <umarbajwa786@gmail.com>
2016-03-25 20:42:59 -0700

    Adds fixed Allergy prompt

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,40 +7,40 @@ users = users.to_i
 #Survey
 
 until users == 0
+  puts "What is your name?"
+  name = gets.chomp
 
-puts "What is your name?"
-name = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp
 
-puts "How old are you?"
-age = gets.chomp
+  puts "What year were you born in?"
+  birth_year = gets.chomp
 
-puts "What year were you born in?"
-birth_year = gets.chomp
+  puts "Would you like some garlic bread?"
+  bread = gets.chomp
 
-puts "Would you like some garlic bread?"
-bread = gets.chomp
+  puts "Would you like to enroll in the company's health insurance?"
+  insurance = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+  hasVampireAllergies = false
+  until hasVampireAllergies
+    puts "Do you have any allergies?"
+    allergies = gets.chomp
+    hasVampireAllergies = allergies == "done" || allergies == "sunshine"
+  end  
+  
 
-puts "Do you have any allergies?"
-allergies = gets.chomp
+  if name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire."
+  elsif insurance == "yes" && bread == "yes" && age = "2016-#{birth_year}"
+    puts "Probably not a vampire."
+  elsif hasVampireAllergies || (insurance == "waives" && bread == "no" && age != "2016-#{birth_year}")
+    puts "Probably a vampire."
+  else
+    puts "Results inconclusive."
+  end
 
-if  
-	name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitely a vampire."
-elsif
-  	insurance == "yes" && bread == "yes" && age = "2016-#{birth_year}"
-  	puts "Probably not a vampire."
-elsif
-  	insurance == "waives" && bread == "no" && age != "2016-#{birth_year}"
-  	puts "Probably a vampire."
-else
-	puts "Results inconclusive."
-end
-
-users -= 1
-
+  users -= 1
 end
 
 #Regrets
