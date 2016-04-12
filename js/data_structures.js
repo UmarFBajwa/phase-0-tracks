c702ee3a4fb77d0e6df2963cbf9eb962c405cca8
Eleanor Roosevelt <umarbajwa786@gmail.com>
2016-04-12 14:21:28 -0700

    Add Release 1, 2 and 3

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,4 +1,4 @@
-// RELEASE 0 & 1
+// RELEASE 0, 1, 2
 // Creating two Arrays with colors and names
 var colors = ["blue", "red", "purple", "green"];
 var names = ["Zoe", "Michael", "Susan", "Casper"];
@@ -21,7 +21,43 @@ for (var i = 0; i < colors.length; i++) {
   // horse['color'] = colors[i];
   // horses.push(horse); 
 
-// Listing the Array in a cleaner format
+// RELEASE 3
+// Loop through Keys and Values
 for (var index = 0; index < horses.length; index++) {
   console.log("The horse " + horses[index]['name'] +" is " + horses[index].color + ".");
 }
+
+// RELEASE 2
+// Create Constructor Function that builds a Car with Type, Color, and Maxspeed
+function Car(type, color, maxspeed) {
+  console.log("Our new car:", this);
+  
+  this.type = type;
+  this.color = color;
+  this.maxspeed = maxspeed;
+
+// Create function Rev that prints Voom, voom...  
+  this.rev = function() {console.log("Voom, voom!"); };
+
+  console.log("Car Initialization Complete");
+}
+
+// Prompt to suggest build a car
+console.log("Let's build a car...")
+
+// Car #1
+var anotherCar = new Car("Ferrari", "Red", 205);
+console.log(anotherCar);
+console.log("Our car can really rev:");
+anotherCar.rev();
+console.log("----");
+
+// Prompt to build another car
+console.log("How about we build another car...");
+
+// Car #2
+var betterCar = new Car("Lambo", "Orange", 225)
+console.log(betterCar);
+console.log("This car can rev even more:");
+betterCar.rev();
+console.log("----");
