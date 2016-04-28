1167b761d22374e7d0341bd3eb5dbb48bd859358
Eleanor Roosevelt <umarbajwa786@gmail.com>
2016-04-27 20:08:48 -0700

    Initial commit

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,25 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a /contact route that displayes an address
+get '/contact' do
+  "633 Folsom St, San Francisco, CA 94..."
+end
+
+# write a /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job
+# [person's name]!". If the query is not present, the route simply says "good job!"
+
+get '/great_job' do 
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+
+
+
+
