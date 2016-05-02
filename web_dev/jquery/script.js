fc4d668f2db44a826540d990287d931f6e9d9e58
UmarFBajwa <umarbajwa786@gmail.com>
2016-05-01 20:45:02 -0700

    Initial commit

---
+++ web_dev/jquery/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $(".box").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").animate({left: '250px'});
+    });
+});
